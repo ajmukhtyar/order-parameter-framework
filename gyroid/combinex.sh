9731bbb2c4b86f6287f23c5a0ccbda78243e742a
ajmukhtyar <49498430+ajmukhtyar@users.noreply.github.com>
2019-09-26 17:15:28 -0400

    Update combinex.sh

--- gyroid/combinex.sh
+++ gyroid/combinex.sh
@@ -1,27 +1,23 @@
+##Calculate bond order parameter Q6 using LAMMPS##
+##Returns a dump file with the Q6 vector for every particle##
+
 #!/bin/bash
-
-
-#LD_LIBRARY_PATH=/usr/local/intel/fc/10.0.023/lib:/usr/lib64:/usr/local/mpich2/icse/lib
-LD_LIBRARY_PATH=/opt/ohpc/pub/software/vmd-1.9.3/lib64:/opt/ohpc/pub/mpi/openmpi3-gnu8/3.1.2/lib:/opt/ohpc/pub/compiler/gcc/8.2.0/lib64:/usr/lib64:/home/fs01/ajm529/lammps-16Mar18/src/
-
 t0=$1
-
-###Get Data####
-sed -e s/SSSS/$t0/g getcoord.tcl > getcoordx.tcl
-sed -e s/SSSS/$t0/g getcoordA.tcl > getcoordAx.tcl
-
 module load vmd/1.9.3
-vmd traj$t0.lammpstrj -dispdev text < getcoordx.tcl
-vmd traj$t0.lammpstrj -dispdev text < getcoordAx.tcl
 
+### Get data for network A ####
+sed -e s/SSSS/$t0/g getcoordA.tcl > getcoordAx.tcl
+vmd traj$t0.lammpstrj -dispdev text < getcoordAx.tcl
 sed -e s/PPPP/$t0/g writedatafile.tcl > writedatafilex.tcl
 vmd A$t0.lammpstrj -dispdev text < writedatafilex.tcl
-
-sed -e s/PPPP/$t0/g B_writedatafile.tcl > B_writedatafilex.tcl
-vmd B$t0.lammpstrj -dispdev text < B_writedatafilex.tcl
-
 sed -e s/QQQQ/$t0/g controlq.dpd > controlx.dpd
 /home/fs01/ajm529/lammps-16Mar18/src/lmp_serial -in controlx.dpd
+
+### Get data for network B ####
+sed -e s/SSSS/$t0/g getcoord.tcl > getcoordx.tcl
+vmd traj$t0.lammpstrj -dispdev text < getcoordx.tcl
+sed -e s/PPPP/$t0/g B_writedatafile.tcl > B_writedatafilex.tcl
+vmd B$t0.lammpstrj -dispdev text < B_writedatafilex.tcl
 sed -e s/QQQQ/$t0/g B_controlq.dpd > B_controlx.dpd
 /home/fs01/ajm529/lammps-16Mar18/src/lmp_serial -in B_controlx.dpd
 
